1c56c253b62890ec58652e15344dc236a3ddf8f1
KEITH SALO <Kethsalo@gmail.com>
2023-02-28 18:06:54 -0800

    Django Admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from cars.models import Car2
 # Register your models here.
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'make', 'year', 'color')
+    list_filter = ('year', 'color')
+    search_fields = ('make', 'year')
+
+
+admin.site.register(Car2, CarAdmin, )
